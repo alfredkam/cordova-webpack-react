0eb37f4299ab7aa094abe75cfcac834f48e158c4
rexk <myungsunrex@gmail.com>
2015-03-13 13:44:15 -0700

    Add style-loader

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 var app = require('./cordova-app');
-
+require('../styles/all.css');
 app.initialize();
 
 app.on('deviceready', function (e) {
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,15 @@ module.exports = {
   },
 
   module: {
-
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
   }
 }
